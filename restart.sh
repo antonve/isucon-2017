495890960f8bf17b9c3670df9dbda755122826ee
Yuki Yamada <yamadayuuki0818@gmail.com>
2017-10-21 14:44:21 +0900

    Enable to analyze log file

--- restart.sh
+++ restart.sh
@@ -1,2 +1,10 @@
-sudo systemctl reload nginx.service
+#!/bin/bash
+
+set -ex
+
+if [ -f /var/log/nginx/access.log ]; then
+  sudo mv /var/log/nginx/access.log ./log/access.log.$(date "+%Y%m%d%H%M")
+fi
+
+sudo systemctl restart isubata.golang.service
 sudo systemctl restart nginx.service
